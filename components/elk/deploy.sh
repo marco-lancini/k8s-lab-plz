014da478c772bfeb75709ecba3f5615a7f729f94
marco-lancini <info@marcolancini.it>
2020-06-27 15:14:58 +0100

    Update deployment script

--- components/elk/deploy.sh
+++ components/elk/deploy.sh
@@ -1,43 +1,42 @@
 #! /bin/bash
 
+NAMESPACE="elastic-system"
+
+wait_pod () {
+status=$(kubectl -n ${NAMESPACE} get pods --selector="${2}" -o json | jq '.items[].status.phase')
+while [ -z "$status" ] || [ $status != '"Running"' ]
+do
+    printf "\t[*] Waiting for $1 to be ready...\n"
+    sleep 5
+    status=$(kubectl -n ${NAMESPACE} get pods --selector="${2}" -o json | jq '.items[].status.phase')
+done
+printf "\t[*] $1 is ready\n"
+}
+
 #
 # Deploying Elastic Operator
 #
-printf "[>] Deploying Elastic Operator...\n"
+printf "[+] Deploying Elastic Operator...\n"
 plz run //components/elk:eck_push
-
-status=$(kubectl -n elastic-system get po elastic-operator-0 -o json | jq '.status.phase')
-while [ -z "$status" ] || [ $status != '"Running"' ]
-do
-    printf "\t[*] Waiting for Elastic Operator to be ready...\n"
-    sleep 5
-    status=$(kubectl -n elastic-system get po elastic-operator-0 -o json | jq '.status.phase')
-done
+wait_pod 'Elastic Operator' 'control-plane=elastic-operator'
 
 #
 # Deploying Elasticsearch cluster
 #
-printf "[>] Deploying Elasticsearch cluster...\n"
+printf "[+] Deploying Elasticsearch cluster...\n"
 plz run //components/elk:elasticsearch_push
-
-status=$(kubectl -n elastic-system get pods --selector='elasticsearch.k8s.elastic.co/cluster-name=elasticsearch' -o json | jq '.items[].status.phase')
-while [ -z "$status" ] || [ $status != '"Running"' ]
-do
-    printf "\t[*] Waiting for Elasticsearch to be ready...\n"
-    sleep 5
-    status=$(kubectl -n elastic-system get pods --selector='elasticsearch.k8s.elastic.co/cluster-name=elasticsearch' -o json | jq '.items[].status.phase')
-done
+wait_pod 'Elasticsearch' 'elasticsearch.k8s.elastic.co/cluster-name=elasticsearch'
 
 #
 # Deploying Kibana instance
 #
-printf "[>] Deploying Kibana instance...\n"
+printf "[+] Deploying Kibana instance...\n"
 plz run //components/elk:kibana_push
+wait_pod 'Kibana' 'kibana.k8s.elastic.co/name=kibana'
 
-status=$(kubectl -n elastic-system get pod --selector='kibana.k8s.elastic.co/name=kibana' -o json | jq '.items[].status.phase')
-while [ -z "$status" ] || [ $status != '"Running"' ]
-do
-    printf "\t[*] Waiting for Kibana to be ready...\n"
-    sleep 5
-    status=$(kubectl -n elastic-system get pod --selector='kibana.k8s.elastic.co/name=kibana' -o json | jq '.items[].status.phase')
-done
+#
+# Deploying Filebeats
+#
+printf "[+] Deploying Filebeats...\n"
+plz run //components/elk:filebeats_push
+wait_pod 'Filebeats' 'k8s-app=filebeat'
