1b286e03a589ddf7efeeeb43cc8a0de910e57b6b
marco-lancini <info@marcolancini.it>
2021-03-30 00:22:59 +0100

    Fix typo

--- components/vault/forward-ui.sh
+++ components/vault/forward-ui.sh
@@ -1,5 +1,18 @@
 #! /bin/bash
 
+TARGET=$1
+if [[ $# -lt 1 ]] ; then
+    TARGE="minikube"
+fi
+if [[ $TARGET == "baremetal" ]]
+then
+    POD="vault-helm-baremetal-0"
+else
+    POD="vault-helm-0"
+fi
+
+
+
 VAULT_ROOT_TOKEN=$(cat cluster-keys.json | jq -r ".root_token")
 printf "[+] Forwarding Vault UI to http://127.0.0.1:8200\n"
 printf "\t[*] Root Token: ${VAULT_ROOT_TOKEN}\n"
