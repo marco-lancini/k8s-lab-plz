05646606d256dc0367911c1839ffa321ae000b19
marco-lancini <info@marcolancini.it>
2021-03-30 11:49:28 +0100

    Add Ingress

--- components/vault/forward-ui.sh
+++ components/vault/forward-ui.sh
@@ -6,14 +6,12 @@ if [[ $# -lt 1 ]] ; then
 fi
 if [[ $TARGET == "baremetal" ]]
 then
-    POD="vault-helm-baremetal-0"
+    SVC="vault-helm-baremetal-ui"
 else
-    POD="vault-helm-0"
+    SVC="vault-helm-ui"
 fi
-
-
 
 VAULT_ROOT_TOKEN=$(cat cluster-keys.json | jq -r ".root_token")
 printf "[+] Forwarding Vault UI to http://127.0.0.1:8200\n"
 printf "\t[*] Root Token: ${VAULT_ROOT_TOKEN}\n"
-kubectl -n vault port-forward svc/vault-helm-ui 8200 &
+kubectl -n vault port-forward svc/${SVC} 8200
