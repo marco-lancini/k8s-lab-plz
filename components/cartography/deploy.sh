16ec20b1e914c59384059e29b2f4ec03b626912c
marco-lancini <info@marcolancini.it>
2021-04-11 16:49:44 +0100

    Deploy script

--- components/cartography/deploy.sh
+++ components/cartography/deploy.sh
@@ -17,8 +17,8 @@ printf "\n[+] Deploying Cartography on ${TARGET}...\n"
 plz run //components/cartography:cartography-namespace_push
 plz run //components/cartography:vault-agent-service-account_push
 
-# Generate Neo4j Password and store it into Vault
-printf "\n[+] Generating Neo4j Password and persisting it into Vault at: ${NEO4J_PASSWORD_LOCATION}\n"
-VAULT_POD_NAME=$(plz run //common:get_resource_from_selector ${VAULT_NAMESPACE} pod ${VAULT_SELECTOR})
-kubectl -n ${VAULT_NAMESPACE} exec ${VAULT_POD_NAME} -it -- vault kv put ${NEO4J_PASSWORD_LOCATION} NEO4J_SECRETS_PASSWORD="$(openssl rand -base64 32)"
+# Setup and Deploy Neo4j
+plz run //components/cartography:deploy-neo4j ${NAMESPACE} ${TARGET} ${SELECTOR}
 
+# Setup and Deploy Cartography
+plz run //components/cartography:deploy-cartography ${NAMESPACE} ${TARGET} ${SELECTOR}
--- docs/cartography.md
+++ docs/cartography.md
@@ -9,4 +9,8 @@ Please refer to [Vault Setup](vault.md) for more information.
 ❯ plz run //components/cartography:deploy [minikube|baremetal]
 ```
 * Setup namespace: creates a `cartography` namespace, and a Vault Agent service account
+* Setup and Deploy Neo4j
+* Setup and Deploy Cartography
+
+
 * Generates a random password for Neo4j and stores it into Vault
