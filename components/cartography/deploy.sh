02fe1b283e14698a0b84e14bb4468ff3fa24a53b
marco-lancini <info@marcolancini.it>
2021-04-23 13:33:15 +0100

    Update deploy

--- components/cartography/deploy.sh
+++ components/cartography/deploy.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
 NAMESPACE="cartography"
-SELECTOR="app=cartography,component=neo4j"
+SELECTOR_NEO4J="app=cartography,component=neo4j"
+SELECTOR_JOB="app=cartography,component=cartography"
 TARGET=$1
 if [[ $# -lt 1 ]] ; then
     TARGET="minikube"
@@ -15,7 +16,7 @@ plz run //components/cartography:cartography-namespace_push
 plz run //components/cartography:vault-agent-service-account_push
 
 # Setup and Deploy Neo4j
-plz run //components/cartography:deploy-neo4j ${NAMESPACE} ${TARGET} ${SELECTOR}
+plz run //components/cartography:deploy-neo4j ${NAMESPACE} ${TARGET} ${SELECTOR_NEO4J}
 
 # Setup and Deploy Cartography
-plz run //components/cartography:deploy-cartography ${NAMESPACE} ${TARGET} ${SELECTOR}
+plz run //components/cartography:deploy-cartography ${NAMESPACE} ${TARGET} ${SELECTOR_JOB}
