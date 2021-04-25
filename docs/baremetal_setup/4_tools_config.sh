320d99b2690e9b70cf82145f0e6c01fa5f03f825
marco-lancini <info@marcolancini.it>
2021-04-25 13:45:34 +0100

    Move docs

--- docs/baremetal_setup/4_tools_config.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-setenforce 0
-sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
-
-systemctl enable --now cri-o && systemctl enable --now kubelet
-echo "KUBELET_EXTRA_ARGS=--cgroup-driver=systemd" | tee /etc/sysconfig/kubelet
