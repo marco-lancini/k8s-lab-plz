59826040efb529cdd985e2c27320e80b09cee9fa
marco-lancini <info@marcolancini.it>
2021-06-21 20:17:38 +0100

    Update deploy script

--- components/elk/deploy.sh
+++ components/elk/deploy.sh
@@ -1,42 +1,24 @@
 #! /bin/bash
 
-NAMESPACE="elastic-system"
+NAMESPACE="elastic"
+TARGET=$1
+if [[ $# -lt 1 ]] ; then
+    TARGET="minikube"
+fi
 
-wait_pod () {
-status=$(kubectl -n ${NAMESPACE} get pods --selector="${2}" -o json | jq '.items[].status.phase')
-while [ -z "$status" ] || [ $status != '"Running"' ]
-do
-    printf "\t[*] Waiting for $1 to be ready...\n"
-    sleep 5
-    status=$(kubectl -n ${NAMESPACE} get pods --selector="${2}" -o json | jq '.items[].status.phase')
-done
-printf "\t[*] $1 is ready\n"
-}
+# Create `elastic` namespace
+printf "\n[+] Creating ${NAMESPACE} namespace...\n"
+plz run //components/elk:elk-namespace_push
 
 #
-# Deploying Elastic Operator
+# Deploying ELK:
+#   - Elasticsearch cluster
+#   - Kibana instance
 #
-printf "[+] Deploying Elastic Operator...\n"
-plz run //components/elk:eck_push
-wait_pod 'Elastic Operator' 'control-plane=elastic-operator'
-
-#
-# Deploying Elasticsearch cluster
-#
-printf "[+] Deploying Elasticsearch cluster...\n"
-plz run //components/elk:elasticsearch_push
-wait_pod 'Elasticsearch' 'elasticsearch.k8s.elastic.co/cluster-name=elasticsearch'
-
-#
-# Deploying Kibana instance
-#
-printf "[+] Deploying Kibana instance...\n"
-plz run //components/elk:kibana_push
-wait_pod 'Kibana' 'kibana.k8s.elastic.co/name=kibana'
-
-#
-# Deploying Filebeats
-#
-printf "[+] Deploying Filebeats...\n"
-plz run //components/elk:filebeats_push
-wait_pod 'Filebeats' 'k8s-app=filebeat'
+printf "\n[+] Deploying ELK on ${TARGET}...\n"
+if [[ $TARGET == "baremetal" ]]
+then
+    plz run //components/elk:elk-baremetal_push
+else
+    plz run //components/elk:elk-minikube_push
+fi
