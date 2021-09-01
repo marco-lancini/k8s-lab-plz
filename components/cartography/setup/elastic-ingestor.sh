eb0dc53a646403901dbade7f3ec3ab1870bdeb9f
marco-lancini <info@marcolancini.it>
2021-09-01 12:16:37 +0100

    Update deploy script

--- components/cartography/setup/elastic-ingestor.sh
+++ components/cartography/setup/elastic-ingestor.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
+NAMESPACE="cartography"
 TARGET=$1
 if [[ $# -lt 1 ]] ; then
     TARGET="minikube"
 fi
+
+#
+# Setup Elastic credentials
+#
+printf "\n[+] Setting up Elastic credentials on ${TARGET}...\n"
+NAMESPACE_ELK="elastic-system"
+ELASTIC_USER="elastic"
+ELASTIC_PASSWORD=$(kubectl -n ${NAMESPACE_ELK} get secrets elasticsearch-es-elastic-user -o=jsonpath='{.data.elastic}' | base64 --decode)
+
+kubectl -n ${NAMESPACE} create secret generic elastic-credentials \
+  --from-literal=username=${ELASTIC_USER} \
+  --from-literal=password=${ELASTIC_PASSWORD}
 
 #
 # Deploy Elastic Ingestor:
